7d880a16b5f9f765078386f55d37ab2a332e9638
grantmacken <grantmacken@gmail.com>
2020-08-12 07:27:31 +1200

    lazy load packer

--- configs/.config/nvim/lua/my/commands.lua
+++ configs/.config/nvim/lua/my/commands.lua
@@ -2,11 +2,11 @@ local M = {}
 M.version = 'v0.0.1'
 
 local cmds = {
-  PackerClean = "packadd packer.nvim | lua require('plugins').clean()";
-  PackerCompile = "packadd packer.nvim | lua require('plugins').compile('~/.config/nvim/plugin/packer_load.vim')";
-  PackerInstall = "packadd packer.nvim | lua require('plugins').install()";
-  PackerSync = "packadd packer.nvim | lua require('plugins').sync()";
-  PackerUpdate = "packadd packer.nvim | lua require('plugins').update()";
+  PackerClean = "packadd packer.nvim | lua require('my/plugins').clean()";
+  PackerCompile = "packadd packer.nvim | lua require('my.plugins').compile('~/.config/nvim/plugin/packer_load.vim')";
+  PackerInstall = "packadd packer.nvim | lua require('my.plugins').install()";
+  PackerSync = "packadd packer.nvim | lua require('my.plugins').sync()";
+  PackerUpdate = "packadd packer.nvim | lua require('my.plugins').update()";
 }
 
 local setCmds = function()
@@ -29,4 +29,4 @@ M.setCmds = setCmds
  - luafile %
 --]]
 --setCmds()
-return M
+return M.setCmds
